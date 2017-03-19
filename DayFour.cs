4603c59a20a909d25197a6ea1facd3b4cb371c0b
madawe <madawe@gmail.com>
2017-03-19 20:49:00 +0900

    30 Days of Code Day 4
    https://www.hackerrank.com/challenges/30-class-vs-instance

---
+++ DayFour.cs
@@ -0,0 +1,36 @@
+﻿class Person
+{
+    public int age;
+    public Person(int initialAge)
+    {
+        // Add some more code to run some checks on initialAge
+
+        age = initialAge;
+
+        if (age < 0)
+        {
+            age = 0;
+            Console.WriteLine("Age is not valid, setting age to 0.");
+        }
+
+    }
+    public void amIOld()
+    {
+        if (age < 13)
+        {
+            Console.WriteLine("You are young.");
+        }
+        if ((age >= 13) && (age < 18))
+        {
+            Console.WriteLine("You are a teenager.");
+        }
+        if (age >= 18)
+        {
+            Console.WriteLine("You are old.");
+        }
+    }
+
+    public void yearPasses()
+    {
+        age++;
+    }
