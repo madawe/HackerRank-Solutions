d9e210f3245d6c1cc40629f47c7b97a336f82b31
madawe <madawe@gmail.com>
2017-03-19 20:50:52 +0900

    30 Days of Code Day 5
    https://www.hackerrank.com/challenges/30-loops

---
+++ DayFive.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args)
+    {
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= 10; i++)
+        {
+
+            Console.Write(N);
+            Console.Write(" x ");
+            Console.Write(i);
+            Console.Write(" = ");
+            Console.WriteLine(N * i);
+        }
+    }
+
+
+}
