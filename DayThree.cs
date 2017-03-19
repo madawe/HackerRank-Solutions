3cc43bbe3719bdd2fd219cfcb4b24d5b666e1e29
madawe <madawe@gmail.com>
2017-03-19 20:46:27 +0900

    30 Days of Code Day 3
    https://www.hackerrank.com/challenges/30-conditional-statements

---
+++ DayThree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int N = Convert.ToInt32(Console.ReadLine());
+        bool done = false;
+        // I decieded to just test for the Not Weird situations, if they don't test true it must be Weird.
+        if (N % 2 == 0 && N > 20)
+        {
+            Console.WriteLine("Not Weird");
+            done = true;
+        }
+        if (N % 2 == 0 && N < 6)
+        {
+            Console.WriteLine("Not Weird");
+            done = true;
+        }
+        if (done != true)
+        {
+            Console.WriteLine("Weird");
+        }
+
+
+    }
+}
