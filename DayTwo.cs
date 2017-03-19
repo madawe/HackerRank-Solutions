8592c5ddc4df8751a006318a7a3bcf8463e2a3b2
madawe <madawe@gmail.com>
2017-03-19 20:38:51 +0900

    30 Days of Code Day Two
    https://www.hackerrank.com/challenges/30-operators

---
+++ DayTwo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        double tipPercent;
+        double taxPercent;
+        double mealCost;
+        double tip, tax, totalCost;
+        mealCost = double.Parse(Console.ReadLine());
+        tipPercent = double.Parse(Console.ReadLine());
+        taxPercent = double.Parse(Console.ReadLine());
+
+        tip = mealCost * ((double)tipPercent / 100);
+        tax = mealCost * ((double)taxPercent / 100);
+        totalCost = (mealCost + tip + tax);
+
+        Console.Write("The total meal cost is ");
+        Console.Write(Math.Round(totalCost));
+        Console.Write(" dollars.");
+    }
+}
