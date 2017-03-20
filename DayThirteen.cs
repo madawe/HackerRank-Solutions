6290803c9cc7c95ad0d6081330ec16a2d85bc999
madawe <madawe@gmail.com>
2017-03-20 15:07:23 +0900

    30 Days of Code Day 13
    https://www.hackerrank.com/challenges/30-abstract-classes

---
+++ DayThirteen.cs
@@ -0,0 +1,17 @@
+﻿//Write MyBook class
+
+class MyBook : Book
+{
+    private int price = 0;
+    public MyBook(string title, string author, int pr) : base(title, author)
+    {
+        this.price = pr;
+    }
+    public override void display()
+    {
+        Console.WriteLine("Title:" + " " + title);
+        Console.WriteLine("Author:" + " " + author);
+        Console.WriteLine("Price:" + " " + price);
+
+    }
+}
