8c7c126ba8c8af49f54be02124952112d45da7d7
madawe <madawe@gmail.com>
2017-03-20 15:02:54 +0900

    30 Days of Code Day 9
    https://www.hackerrank.com/challenges/30-recursion
    
    Recursive Factorialing

---
+++ DayNine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int number = Convert.ToInt32(Console.ReadLine());
+        long fact = Factorial(number);
+        Console.WriteLine(fact);
+        Console.ReadKey();
+    }
+    //Recursive factorialing. 
+    private static long Factorial(int number)
+    {
+        if (number == 0)
+        {
+            return 1;
+        }
+        return number * Factorial(number - 1);
+    }
+}
