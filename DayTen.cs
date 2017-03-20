5f2d7cf1ec0566eaf71eeab3fecae461d7820097
madawe <madawe@gmail.com>
2017-03-20 15:03:57 +0900

    30 Days of Code Day 10
    https://www.hackerrank.com/challenges/30-binary-numbers

---
+++ DayTen.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        //my code
+
+        string binaryN = Convert.ToString(n, 2);
+        //Console.WriteLine(binaryN);
+
+        int max = 1;
+        int currentMax = 0;
+        foreach (char bin in binaryN)
+        {
+            if (bin == '1')
+            {
+                currentMax = currentMax + 1;
+            }
+            else
+            {
+                if (currentMax >= max)
+                {
+                    max = currentMax;
+                }
+                currentMax = 0;
+            }
+        }
+        if (currentMax > max)
+        {
+            max = currentMax;
+            currentMax = 0;
+        }
+        Console.WriteLine(max);
+        //end my code 
+    }
+}
